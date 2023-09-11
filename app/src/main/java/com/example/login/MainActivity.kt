4c02cb321645838de9ed819f3f88b5161aa6e5d2
Anbs12 <118308826+Anbs12@users.noreply.github.com>
2023-09-11 20:52:10 -0300

    actualizacion 1

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -18,6 +18,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Anubis
+
         usuarioEditText = findViewById(R.id.usuarioText)
         passwordEditText = findViewById(R.id.passText)
         loginButton = findViewById(R.id.btnIngresar)
